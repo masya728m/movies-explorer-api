c09355b2fcbbb3c61e5e2c52e8e13be8abd44222
marina <masya728m@gmail.com>
2022-01-17 23:50:17 +0300

    выполнена часть user API

---
+++ models/movie.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const movieSchema = mongoose.Schema({
+  country: {
+    type: String,
+    required: true
+  },
+  director: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  year: {
+    type: Date,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validator.isURL
+    }
+  },
+  trailer: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validator.isURL
+    }
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validator.isURL
+    }
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true
+  },
+  movieId: {
+    type: String,
+    required: true
+  },
+  nameRU: {
+    type: String,
+    required: true
+  },
+  nameEN: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('movie', movieSchema);
---
+++ routes/users.js
@@ -0,0 +1,25 @@
+const router = require('express')
+  .Router();
+const {
+  celebrate,
+  Joi
+} = require('celebrate');
+const {
+  getUser,
+  updateUserProfile
+} = require('../controllers/users');
+
+router.get('/me', getUser);
+router.patch('/me', celebrate({
+  body: Joi.object()
+    .keys({
+      name: Joi.string()
+        .required()
+        .min(2),
+      email: Joi.string()
+        .required()
+        .email()
+    })
+}), updateUserProfile);
+
+module.exports = router;
