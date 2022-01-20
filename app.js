1d6df8e9f47e985efc30ed708844b97730d1579e
marina <masya728m@gmail.com>
2022-01-20 23:33:23 +0300

    испотзуется база данных moviesdb

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const NotFoundError = require('./errors/notFoundError');
 
 const {
   PORT = 3000,
-  DB_URL = 'mongodb://127.0.0.1:27017/',
+  DB_URL = 'mongodb://127.0.0.1:27017/moviesdb',
 } = process.env;
 
 const app = express();
